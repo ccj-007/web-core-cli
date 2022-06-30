1bae4f57f90d6e85931fb87f0946c84af6b31b70
陈蔡俊 <chencaijun@mx2.zzss.com>
2022-06-30 18:03:35 +0800

    完成递归文件拷贝

--- bin/cli.js
+++ bin/cli.js
@@ -2,26 +2,38 @@
 const program = require('commander')
 const chalk = require('chalk')
 const ora = require('ora')
+const figlet = require('figlet');
 
+//quan-h5cli create my-project
 program
+  .on('--help', () => {
+    // 新增说明信息
+    console.log('\r\n' + figlet.textSync('qunaH5', {
+      font: 'Ghost',
+      horizontalLayout: 'default',
+      verticalLayout: 'default',
+      width: 80,
+      whitespaceBreak: true
+    }));
+    // 新增说明信息
+    console.log(`\r\nRun ${chalk.cyan(`roc <command> --help`)} show details\r\n`)
+  })
   .version('0.1.0')
   .command('create <name>')
   .description('create a new project')
   .option('-f, --force', 'overwrite target directory if it exist')
   .action((name, options) => {
-    require('../lib/create.js')(name, options)
-    //配置
-    // const spinner = ora('Loading unicorns').start();
-    // setTimeout(() => {
-    //   spinner.color = 'yellow';
-    //   spinner.text = 'Loading rainbows';
+    //转圈圈
+    const spinner = ora('Loading unicorns').start();
+    spinner.color = 'yellow';
+    spinner.text = 'Loading rainbows';
 
-    //   setTimeout(() => {
-    //     console.log(chalk.cyan.bold(`project name is ${name}`))
-    //   }, 2000);
-    // }, 2000);
-    // 打印命令行输入的值
+    //开始流程
+    require('../lib/create.js')(name, options)
+
+    spinner.stop()
   })
+
 
 program
   // 配置版本号信息
